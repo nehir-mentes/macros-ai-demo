7653d068e534970a009830d4be24abab06a93dc7
nehir-mentes <nehirmen.98@gmail.com>
2025-04-28 20:02:05 +0000

    images

--- app/controllers/macros_controller.rb
+++ app/controllers/macros_controller.rb
@@ -9,6 +9,26 @@ class MacrosController < ApplicationController
     # or you can handle nulls with @the_image = params.fetch("image_param", nil)
     @the_description = params.fetch("description_param")
     
+
+c = OpenAI::Chat.new
+
+c.system("You are a nutritionist.The user will give you an image and/or a description of a meal. Your job is to estimate the amount of macronutrients and calories in it.")
+
+c.schema = '{"name": "nutrition_values","strict": true,"schema": {"type": "object","properties": {  "fat": {    "type": "number",    "description": "The amount of fat in grams."  },  "protein": {    "type": "number",    "description": "The amount of protein in grams."  },  "carbs": {    "type": "number",    "description": "The amount of carbohydrates in grams."  },  "total_calories": {    "type": "number",    "description": "The total calories calculated based on fat, protein, and carbohydrates."  }},"required": [  "fat",  "protein",  "carbs",  "total_calories"],"additionalProperties": false}}'
+
+c.user("Here is an image", image: @the_image)
+
+c.user(@the_description)
+
+@structured_output = c.assistant!
+
+@g_carbs = @structured_output.fetch("carbs")
+@g_protein = @structured_output.fetch("protein")
+@g_fat = @structured_output.fetch("fat")
+@total_calories = @structured_output.fetch("total_calories")
+
+c.assistant!
+
     render({ :template => "macros_templates/process_form" })
   end
 
