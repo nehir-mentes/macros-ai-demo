e55ec3117d872a4ce95bcd5fb2188875da7e13c2
nehir-mentes <nehirmen.98@gmail.com>
2025-04-28 20:31:10 +0000

    semi-draft

--- app/controllers/macros_controller.rb
+++ app/controllers/macros_controller.rb
@@ -11,21 +11,64 @@ class MacrosController < ApplicationController
     
 
 c = OpenAI::Chat.new
+c.model = "o3"
 
 c.system("You are a nutritionist.The user will give you an image and/or a description of a meal. Your job is to estimate the amount of macronutrients and calories in it.")
 
-c.schema = '{"name": "nutrition_values","strict": true,"schema": {"type": "object","properties": {  "fat": {    "type": "number",    "description": "The amount of fat in grams."  },  "protein": {    "type": "number",    "description": "The amount of protein in grams."  },  "carbs": {    "type": "number",    "description": "The amount of carbohydrates in grams."  },  "total_calories": {    "type": "number",    "description": "The total calories calculated based on fat, protein, and carbohydrates."  }},"required": [  "fat",  "protein",  "carbs",  "total_calories"],"additionalProperties": false}}'
+c.schema = {
+  "name": "nutrition_values",
+  "strict": true,
+  "schema": {
+    "type": "object",
+    "properties": {
+      "fat": {
+        "type": "number",
+        "description": "The amount of fat in grams."
+      },
+      "protein": {
+        "type": "number",
+        "description": "The amount of protein in grams."
+      },
+      "carbs": {
+        "type": "number",
+        "description": "The amount of carbohydrates in grams."
+      },
+      "total_calories": {
+        "type": "number",
+        "description": "The total calories calculated based on fat, protein, and carbohydrates."
+      },
+      "notes": {
+        "type": "string",
+        "description": "A breakdown of how you arrived at the values, and additional notes."
+      }
+    },
+    "required": [
+      "fat",
+      "protein",
+      "carbs",
+      "total_calories",
+      "notes"
+    ],
+    "additionalProperties": false
+  }
+}
 
 c.user("Here is an image", image: @the_image)
 
 c.user(@the_description)
 
-@structured_output = c.assistant!
+# Now convert it to JSON string
+structured_input_json = structured_input.to_json
+
+# Then pass it
+@structured_output = c.assistant!(structured_input: structured_input_json)
+
 
 @g_carbs = @structured_output.fetch("carbs")
 @g_protein = @structured_output.fetch("protein")
 @g_fat = @structured_output.fetch("fat")
 @total_calories = @structured_output.fetch("total_calories")
+@notes = @structured_output.fetch("notes")
 
 c.assistant!
 
